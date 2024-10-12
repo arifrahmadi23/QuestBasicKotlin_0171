2d003907ef5c4f16f04a18f54c279cf52fc84623
arifrahmadi23 <muhammad231arif@gmail.com>
2024-10-12 21:49:38 +0700

    Commit Map

--- app/src/main/java/com/example/tes_as/Collections.kt
+++ app/src/main/java/com/example/tes_as/Collections.kt
@@ -59,3 +59,39 @@ fun ContohSet() {
     println(shapesLocked)
 }
 
+// Map adalah Kumpulan data yang disusun dalam pasangan key-value
+// Map bersifat undorded, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Map bersifat mutable dan read only, artinya data yag dimasukkan dapat diubah dan tidak dapat diubah
+// Map Read-only menggunakan mapOf
+// Map Mutable menggunakan mutable MapOf
+
+fun ContohMap() {
+
+    println()
+    println("=== Map ===")
+
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shapep
+    println(shapesLocked)
+}
+
