6694ec9edbc1a36bf5b338c448dc60e287dec43f
arifrahmadi23 <muhammad231arif@gmail.com>
2024-10-12 22:16:19 +0700

    Membuat fungsi perulangan pada ConditionalStatement

--- app/src/main/java/com/example/tes_as/ConditionalExpression.kt
+++ app/src/main/java/com/example/tes_as/ConditionalExpression.kt
@@ -38,6 +38,33 @@ fun ConditionalStatement() {
     catch (e: NumberFormatException){
         println("Invalid Number")
     }
+}
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda" , "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    }while (y <= 5)
 
 }
+
 
